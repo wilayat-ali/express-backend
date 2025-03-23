2188dd820ffd141f3275b70dad20e9fe3133ca90
wilayat8070 <wilayat@360i.agency>
2025-03-23 18:52:52 +0500

    .env add

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,9 @@
+const users = [
+  { id: 1, name: "John Doe", email: "john@example.com" },
+  { id: 2, name: "Jane Doe", email: "jane@example.com" },
+];
+
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Serverless API!" });
+  res.status(200).json(users);
 }
+
